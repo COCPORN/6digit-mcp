41021bd98062c51dbb4bd9961aa1ef4d9f832acf
Lars Thomas Denstad <ungedenstad@gmail.com>
2025-03-09 22:01:41 +0100

    Update build

--- build/index.js
+++ build/index.js
@@ -1,4 +1,5 @@
 import { McpServer, ResourceTemplate, } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 const server = new McpServer({
     name: "6digit echo",
@@ -26,3 +27,5 @@ server.prompt("echo", { message: z.string() }, ({ message }) => ({
         },
     ],
 }));
+const transport = new StdioServerTransport();
+await server.connect(transport);
